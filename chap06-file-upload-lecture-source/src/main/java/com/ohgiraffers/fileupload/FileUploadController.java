6bdccdfd9323e34a311b42dd3b7c61408ee5e462
seongeun223 <jklun080921@gmail.com>
2024-07-11 10:57:51 +0900

    file upload

---
+++ chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.fileupload;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@Slf4j // 로그
+public class FileUploadController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @PostMapping("/single-file")
+    public String singleFileUpload(@RequestParam MultipartFile singleFile,
+                                   @RequestParam String singleFileDescription,
+                                   Model model) {
+
+        log.info("[FileUploadController] singleFileUpload : =====> {}", singleFile);
+        log.info("singleFileDescription : =======> {}", singleFileDescription);
+
+        return "";
+
+    }
+}
