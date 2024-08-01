96a952092f079b279651a658dd0d1e35665a3439
seongeun223 <jklun080921@gmail.com>
2024-08-01 13:00:02 +0900

    thymeleaf

---
+++ chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/model/dto/SelectCriteria.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.thymeleaf.model.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class SelectCriteria {
+    private int startPage;
+    private int endPage;
+    private int pageNo;
+}
